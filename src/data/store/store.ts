a19e30cd75ebee4c53225d61b583744c38d5bf85
nutawat rodsomboon <68809531+salmonindaeyo@users.noreply.github.com>
2025-03-14 15:47:30 +0700

    hidden store

--- src/data/store/store.ts
+++ src/data/store/store.ts
@@ -1,31 +1,31 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+// import { create } from "zustand";
+// import { devtools } from "zustand/middleware";
 
-export type State = {
-  count: number;
-  count2: number;
-};
+// export type State = {
+//   count: number;
+//   count2: number;
+// };
 
-type Actions = {
-  inc: () => void;
-  inc2: () => void;
-};
+// type Actions = {
+//   inc: () => void;
+//   inc2: () => void;
+// };
  
-const countStore = create<State & Actions>()(
-  devtools(
-    (set) => ({
-      count: 1,
-      count2: 0,
-      inc: () => set((state) => ({ count: state.count + 1 })),
-      inc2: async () => {
-        // we can fetching data here
-        set({ count2: 500 });
-      },
-    }),
-    {
-      name: 'Count Store',
-    }
-  )
-);
+// const countStore = create<State & Actions>()(
+//   devtools(
+//     (set) => ({
+//       count: 1,
+//       count2: 0,
+//       inc: () => set((state) => ({ count: state.count + 1 })),
+//       inc2: async () => {
+//         // we can fetching data here
+//         set({ count2: 500 });
+//       },
+//     }),
+//     {
+//       name: 'Count Store',
+//     }
+//   )
+// );
 
-export default countStore;
+// export default countStore;
